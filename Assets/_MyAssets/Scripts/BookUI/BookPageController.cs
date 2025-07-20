4b9087188ec1a0565acf25ce2858dddc84d965c0
manato1201 <manato1201m@icloud.com>
2025-07-21 07:33:48 +0900

    feature:液体シェーダー導入

---
+++ Assets/_MyAssets/Scripts/BookUI/BookPageController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
+
+public class BookPageController : MonoBehaviour
+{
+    [SerializeField] private BookUI bookUI;
+    [SerializeField] private Button nextButton;
+    [SerializeField] private Button prevButton;
+
+    void Start()
+    {
+        if (nextButton != null)
+            nextButton.onClick.AddListener(GoToNextPage);
+
+        if (prevButton != null)
+            prevButton.onClick.AddListener(GoToPreviousPage);
+    }
+
+    public void GoToNextPage()
+    {
+        bookUI.CurrentPage++;
+    }
+
+    public void GoToPreviousPage()
+    {
+        bookUI.CurrentPage--;
+    }
+}
