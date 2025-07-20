552d36639857cbd45a5b53638363485d9ad89222
AbeJunsei <153270000+Ajun5GIT@users.noreply.github.com>
2025-07-20 13:14:06 +0900

    feat: 偶数禁止

--- Assets/_MyAssets/Scripts/Utilities/Manual/NoEvenNumber.cs
+++ Assets/_MyAssets/Scripts/Utilities/Manual/NoEvenNumber.cs
@@ -4,7 +4,46 @@ namespace Scripts.Utilities
     {
         private static bool Check_NoEvenNumber(this string text)
         {
-            return false;
+            int i = 0;
+            while (i < text.Length)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    int start = i;
+                    while (i < text.Length && char.IsDigit(text[i])) i++;
+
+                    string numberStr = text.Substring(start, i - start);
+                    numberStr = ConvertToHalfWidth(numberStr);
+
+                    if (int.TryParse(numberStr, out int number))
+                    {
+                        if (number % 2 == 0) return false;
+                    }
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return true;
+        }
+
+        private static string ConvertToHalfWidth(string input)
+        {
+            var sb = new System.Text.StringBuilder(input.Length);
+            foreach (char c in input)
+            {
+                if (c >= '０' && c <= '９')
+                {
+                    sb.Append((char)(c - '０' + '0'));
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
         }
     }
 }
