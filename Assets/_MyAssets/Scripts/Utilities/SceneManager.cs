3c8091295fb10ea680b3d386c93565c4eaa4350b
foriver4725 <hrt67@icloud.com>
2025-07-20 17:07:19 +0900

    feat: Resultからのシーンを追加

--- Assets/_MyAssets/Scripts/Utilities/SceneManager.cs
+++ Assets/_MyAssets/Scripts/Utilities/SceneManager.cs
@@ -6,15 +6,28 @@ namespace Scripts.Utilities
     {
         Title,
         Main,
-        Result, // クリア、オーバー、磔の刑、出世エンディング
+        Result, // 一旦Resultに遷移し、その後対応するシーンに遷移
+        Result_Clear,
+        Result_Complete,
+        Result_Over,
+        Result_Death,
     }
 
     public static class SceneManager
     {
         public static void LoadAsync(this SceneId sceneId)
+            => UniSceneManager.LoadSceneAsync(sceneId.ToStr());
+
+        private static string ToStr(this SceneId sceneId) => sceneId switch
         {
-            string sceneName = sceneId.ToString();
-            UniSceneManager.LoadSceneAsync(sceneName);
-        }
+            SceneId.Title => "Title",
+            SceneId.Main => "Main",
+            SceneId.Result => "Result",
+            SceneId.Result_Clear => "Clear",
+            SceneId.Result_Complete => "Complete",
+            SceneId.Result_Over => "Over",
+            SceneId.Result_Death => "Death",
+            _ => string.Empty,
+        };
     }
 }
