c5a6a541011a1ed2aad49f7a32ff0ede74d858c5
AbeJunsei <153270000+Ajun5GIT@users.noreply.github.com>
2025-07-20 12:59:28 +0900

    feat: 5文字以上連続するひらがな禁止

--- Assets/_MyAssets/Scripts/Utilities/Manual/NoContinuousHiragana.cs
+++ Assets/_MyAssets/Scripts/Utilities/Manual/NoContinuousHiragana.cs
@@ -4,7 +4,24 @@ namespace Scripts.Utilities
     {
         private static bool Check_NoContinuousHiragana(this string text)
         {
-            return false;
+            int count = 0;
+
+            foreach (char c in text)
+            {
+                if (IsHiragana(c))
+                {
+                    count++;
+                    if (count >= 5) return false;
+                }
+                else count = 0;
+            }
+
+            return true;
+        }
+
+        private static bool IsHiragana(char c)
+        {
+            return c >= '\u3040' && c <= '\u309F';
         }
     }
 }
