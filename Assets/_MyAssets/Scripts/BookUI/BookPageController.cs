1be2c373525368e0e9473dcb0d365a0353df44db
manato1201 <manato1201m@icloud.com>
2025-07-21 11:58:02 +0900

    feature:MainSceneUI配置

--- Assets/_MyAssets/Scripts/BookUI/BookPageController.cs
+++ Assets/_MyAssets/Scripts/BookUI/BookPageController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using System;
+using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UI.Extensions;
 
@@ -8,22 +10,29 @@ public class BookPageController : MonoBehaviour
     [SerializeField] private Button nextButton;
     [SerializeField] private Button prevButton;
 
+    private bool isProcessing = false;
+
     void Start()
     {
         if (nextButton != null)
-            nextButton.onClick.AddListener(GoToNextPage);
+            nextButton.onClick.AddListener(() => GoToPageAsync(1).Forget());
 
         if (prevButton != null)
-            prevButton.onClick.AddListener(GoToPreviousPage);
+            prevButton.onClick.AddListener(() => GoToPageAsync(-1).Forget());
     }
 
-    public void GoToNextPage()
+    private async UniTaskVoid GoToPageAsync(int direction)
     {
-        bookUI.CurrentPage++;
-    }
+        if (isProcessing) return;
 
-    public void GoToPreviousPage()
-    {
-        bookUI.CurrentPage--;
+        isProcessing = true;
+
+        // ページを進める/戻す
+        bookUI.CurrentPage += direction;
+
+        // ページ遷移アニメーションなどの終了待機（0.5秒は仮。必要に応じて変更）
+        await UniTask.Delay(TimeSpan.FromSeconds(0.5f));
+
+        isProcessing = false;
     }
 }
