603766d268c327861746acf22b6ce648bda61a77
foriver4725 <hrt67@icloud.com>
2025-07-21 11:51:19 +0900

    feat: Deathシーンの演出追加

--- Assets/_MyAssets/Scripts/Scenes/Result/DeathSceneButtonManager.cs
+++ Assets/_MyAssets/Scripts/Scenes/Result/DeathSceneButtonManager.cs
@@ -1,25 +1,32 @@
 using UnityEngine;
-using UnityEngine.UI;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
-using Sirenix.OdinInspector;
 using Scripts.Utilities;
 using Scripts.Utilities.Buttons;
-using Ct = System.Threading.CancellationToken;
 using Scripts.ScriptableObjects;
+using Ct = System.Threading.CancellationToken;
 
 namespace Scripts.Scenes.Result
 {
     public sealed class DeathSceneButtonManager : ATextButtonManager
     {
-        [SerializeField] private Image bg;
+        [SerializeField] private GameObject bg;
         [SerializeField] private AudioSource bgmAs;
 
+        private void Start() => AnimBg(destroyCancellationToken).Forget();
         protected sealed override void OnClickSucceeded() => GoToTitleScene(destroyCancellationToken).Forget();
 
-        private void Start()
+        private async UniTask AnimBg(Ct ct)
         {
-            bgmAs.Raise(SSound.Entity.DeathBGM, SoundType.BGM);   
+            BackgroundImage.enabled = false;
+            Text.enabled = false;
+            bg.transform.SetScaleX(0);
+            await 0.5f.SecAwait(ct);
+            await bg.transform.DOScaleX(1, 0.3f).SetEase(Ease.InBounce).WithCancellation(ct);
+            BackgroundImage.enabled = true;
+            Text.enabled = true;
+
+            0.2f.SecAwaitThenDo(() => bgmAs.Raise(SSound.Entity.TitleBGM, SoundType.BGM), ct).Forget();
         }
 
         private async UniTask GoToTitleScene(Ct ct)
@@ -28,7 +35,7 @@ namespace Scripts.Scenes.Result
             Text.enabled = false;
 
             await bg.transform.DOScaleX(0, 0.15f).SetEase(Ease.InBounce).WithCancellation(ct);
-            await 1.0f.SecAwait(ct);
+            await 0.5f.SecAwait(ct);
 
             SceneId.Title.LoadAsync();
         }
